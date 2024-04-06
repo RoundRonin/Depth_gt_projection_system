0e307fe6a6d534293fe1d97b1884d68deee944a5
RoundRonin <roundronin8@gmail.com>
2024-04-06 08:49:07 +0300

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -77,25 +77,48 @@ class Image {
 
                 cv::Point current(j, i); // x, y
                 cv::Mat output = paint(image, objects, z_limit, current, id);
-                int dots = countNonZero(output);
-                if (dots >= minDots)
-                    masks.push_back({output, dots});
+                mask_mats.push_back(output);
+                // try {
+                //     /* code */
+                //     int dots = countNonZero(output);
+                //     if (dots >= minDots)
+                //         masks.push_back({output, dots});
+                // } catch (const std::exception &e) {
+                //     std::cerr << e.what() << '\n';
+                // }
             }
         }
 
-        std::sort(masks.begin(), masks.end(),
-                  [](auto const &t1, auto const &t2) {
-                      return get<1>(t1) < get<1>(t2);
-                  });
+        // std::sort(masks.begin(), masks.end(),
+        //           [](auto const &t1, auto const &t2) {
+        //               return get<1>(t1) > get<1>(t2);
+        //           });
 
-        for (auto var : masks) {
-            cout << get<1>(var);
-            cout << endl;
-        }
+        // for (auto var : masks) {
+        //     cout << get<1>(var);
+        //     cout << endl;
+        // }
 
         for (int i = 0; i < maxObjects; i++) {
-            mask_mats.push_back(get<0>(masks.at(i)));
-            imwrite("mask " + to_string(i) + " .png", get<0>(masks.at(i)));
+            // mask_mats.push_back(get<0>(masks.at(i)));
+            // imwrite("mask " + to_string(i) + " .png", get<0>(masks.at(i)));
+            cv::Mat masked;
+            cv::bitwise_and(mask_mats.at(i), mask_mats.at(i), masked, objects);
+
+            // CLEAN UP!! TODO remove
+            int erosion_size = 3;
+
+            int erosion_type = MORPH_ELLIPSE;
+            Mat element = getStructuringElement(
+                erosion_type, Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+                Point(erosion_size, erosion_size));
+
+            cv::Mat output;
+            cv::erode(masked, output, element);
+            cv::dilate(output, output, element);
+            mask_mats.at(i) = output;
+
+            imwrite("mask " + to_string(i) + " .png", mask_mats.at(i));
         }
 
         imwrite("objects.png", objects);
@@ -131,13 +154,13 @@ class Image {
         // cout << endl;
 
         // cout << current.x << " " << current.y << endl;
-        objects.at<uchar>(current) = id;       // Painting the pixel
-        output.at<uchar>(current) = UCHAR_MAX; // Painting the pixel
+        objects.at<uchar>(current) = id; // Painting the pixel
+        output.at<uchar>(current) = id;  // Painting the pixel
 
         // cout << "pushing..." << endl;
         path.push_back(current);
         // recurse
-        cout << "recursing..." << endl;
+        // cout << "recursing..." << endl;
         for (int i = 0; i < 4; i++) {
             int x = directions[i][0];
             int y = directions[i][1];
@@ -159,6 +182,7 @@ class Image {
 
     cv::Mat paint(cv::Mat &image, cv::Mat &objects, int z_limit,
                   cv::Point start, uchar id) {
+        // cv::Mat output = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
         cv::Mat output = cv::Mat(image.size(), image.type());
         vector<cv::Point> path;
         uchar start_z = image.at<uchar>(start);
@@ -173,8 +197,8 @@ class Templates {
 
     int chessboardSize = 20; // Size of each square in the chessboard
     int filler = 2 * chessboardSize;
-    int speedX = 1;
-    int speedY = 1;
+    // int speedX = 1;
+    // int speedY = 1;
 
     int width = 1280;
     int height = 720;
@@ -185,21 +209,19 @@ class Templates {
         height = mask.rows;
     }
 
-    cv::Mat gradient(int iter, cv::Mat mask) {
+    cv::Mat gradient(int iter, cv::Mat mask, int a) {
         Mat frame = Mat::zeros(height, width, CV_8UC3);
-        uchar r = 255 * iter / (10 * 60);
-        uchar g = 255 * (10 * 60 - iter) / (10 * 60);
+        uchar r = 255 * iter * a / (10 * 60);
+        uchar g = 255 * (10 * 60 - iter * a) / (10 * 60);
         uchar b = 0;
 
         if (iter >= 5 * 60) {
-            r = 0;
-            b = 255 * (iter - 5 * 60) / (5 * 60);
+            r = 0 + a * iter * 10;
+            b = 255 * (iter * a - 5 * 60) / (5 * 60);
         }
 
-        rectangle(frame, Point(0, 0), Point(width / 2, height / 2),
-                  Scalar(b, g, r), -1);
-        rectangle(frame, Point(width / 2, width / 2), Point(width, height),
-                  Scalar(r, g, r), -1);
+        rectangle(frame, Point(0, 0), Point(width, height), Scalar(b, g, r),
+                  -1);
 
         cv::Mat masked;
         cv::bitwise_and(frame, frame, masked, mask);
@@ -207,7 +229,7 @@ class Templates {
         return masked;
     }
 
-    cv::Mat chessBoard(int iter, cv::Mat mask) {
+    cv::Mat chessBoard(int iter, cv::Mat mask, int speedX = 1, int speedY = 1) {
         Mat frame = Mat::zeros(height, width, CV_8UC3);
 
         int offsetX = -(filler) + iter * speedX % (filler);
@@ -249,9 +271,7 @@ int main(int argc, char **argv) {
     Image image(argv[1]);
 
     image.erode(5, 5);
-    image.write("e.png");
     image.dilate(5, 5);
-    image.write("e_d.png");
 
     image.erode(3, 3);
     image.dilate(3, 3);
@@ -259,7 +279,9 @@ int main(int argc, char **argv) {
     image.erode(2, 2);
     image.dilate(2, 2);
 
-    image.write("e_d_e_d_e_d.png");
+    image.dilate(3, 3);
+
+    image.write("e_d_e_d_e_d_d.png");
     image.findObjects(atoi(argv[2]));
 
     int width = image.image.cols;
@@ -273,9 +295,21 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    vector<cv::Mat> masks = image.mask_mats;
     Templates templates(image.image);
     for (int i = 0; i < 10 * 60; i++) { // 10 seconds at 60 fps
-        video.write(templates.chessBoard(i, image.image));
+
+        vector<cv::Mat> results;
+        results.push_back(templates.chessBoard(i, masks.at(1), 2, 1));
+        results.push_back(templates.chessBoard(i, masks.at(2), 1, 3));
+        results.push_back(templates.gradient(i, masks.at(3), 1));
+        results.push_back(templates.gradient(i, masks.at(4), 5));
+
+        cv::add(results.at(0), results.at(1), results.at(1));
+        cv::add(results.at(1), results.at(2), results.at(2));
+        cv::add(results.at(2), results.at(3), results.at(3));
+
+        video.write(results.at(3));
     }
 
     video.release();
